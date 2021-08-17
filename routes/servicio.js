49bb67f9b63b55a74821d5080583e5856f325019
JAzzad5 <carlosjosue0114@gmail.com>
2021-08-16 21:12:00 -0600

    [ADD] get user by id

--- routes/servicio.js
+++ routes/servicio.js
@@ -15,4 +15,18 @@ router.get('/all', function( req, res ){
     });
 });
 
+
+router.get('/:id', function( req, res ){
+    let id =  req.params.id;
+    Servicio.find({ _id: id })
+    .then( result =>{
+        res.send(result);
+        res.end();
+    })
+    .catch( error =>{
+        res.send(error);
+        res.end();
+    });
+});
+
 module.exports = router;
